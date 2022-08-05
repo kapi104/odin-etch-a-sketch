b9b7118a126f11c60ed89d63d17d082605c42f66
kapi104 <skorupa7979@gmail.com>
2022-08-05 16:11:34 +0200

    created function for changing number of sqares

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
 let con = document.querySelector('.container');
 
-let side = 16;
-let size = Math.floor(900 / side) + 'px';
 
-let start = function () {
+let start = function (side) {
+    let size = 900 / side + 'px';
     for (let vert = 0; vert < side; vert ++) {
     for (i = 0; i < side; i ++) {
         let div = document.createElement('div');
         div.style.width = size;
         div.style.height = size;
         div.classList.add('sq');
-        if (i === 0) div.classList.add('clear');
         con.appendChild(div);
     }
 }
+return
+}
+
+let draw = function () {
+    let sq = document.querySelectorAll('.sq');
+    sq.forEach(sqare => sqare.addEventListener('mouseover', changeColor));
 }
 
 
-let sq = document.querySelectorAll('.sq');
+
+let btn = document.querySelector('button');
+    btn.addEventListener('click', () => {
+    let side = prompt('how many sqares do you want one side to have? (max 100)');
+    if (typeof(side === 'number') && side <= 100) start(side);
+    else alert('enter correct number');
+    draw();
+    return;
+}, {once: true})
+
 
 let changeColor = function () {
     this.classList.add('red')
 }
-
-sq.forEach(sqare => sqare.addEventListener('mouseover', changeColor))
-
