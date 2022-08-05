adf5bd398729613090de1dd280242aa24a181bf3
kapi104 <skorupa7979@gmail.com>
2022-08-05 16:33:11 +0200

    added reset function

--- script.js
+++ script.js
@@ -20,16 +20,20 @@ let draw = function () {
     sq.forEach(sqare => sqare.addEventListener('mouseover', changeColor));
 }
 
-
+let reset = function () {
+    let sq = document.querySelectorAll('.sq');
+    sq.forEach(item => con.removeChild(item))
+}
 
 let btn = document.querySelector('button');
     btn.addEventListener('click', () => {
+    reset();
     let side = prompt('how many sqares do you want one side to have? (max 100)');
     if (typeof(side === 'number') && side <= 100) start(side);
     else alert('enter correct number');
     draw();
     return;
-}, {once: true})
+})
 
 
 let changeColor = function () {
