14662f1af68892d431ed893b8281ce49c717b096
kapi104 <skorupa7979@gmail.com>
2022-08-05 15:09:28 +0200

    changed structure to flex, added change size button

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 let con = document.querySelector('.container');
 
-for (let vert = 0; vert < 16; vert ++) {
-    for (i = 0; i < 16; i ++) {
+let side = 16;
+let size = Math.floor(900 / side) + 'px';
+
+let start = function () {
+    for (let vert = 0; vert < side; vert ++) {
+    for (i = 0; i < side; i ++) {
         let div = document.createElement('div');
+        div.style.width = size;
+        div.style.height = size;
         div.classList.add('sq');
         if (i === 0) div.classList.add('clear');
         con.appendChild(div);
     }
 }
+}
+
 
 let sq = document.querySelectorAll('.sq');
 
@@ -16,3 +24,4 @@ let changeColor = function () {
 }
 
 sq.forEach(sqare => sqare.addEventListener('mouseover', changeColor))
+
